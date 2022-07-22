cf791d158513299aed16c7d9f87e7aaba6c7f373
Justin Muir <justinmuir@gmail.com>
2022-07-21 21:13:37 -0400

    added second warehouse list route

--- src/Pages/Inventory/InventoryPage.js
+++ src/Pages/Inventory/InventoryPage.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Footer from "../../Components/Footer/Footer";
 import PageHeader from "../../Components/Header/Header";
 
@@ -8,14 +8,12 @@ function InventoryPage()  {
     return (<>
         <PageHeader isActive='Inventory' />
         <div className="component-wrapper">
-            <BrowserRouter>
                 <Switch>
                     <Route path='/inventory' exact component={Footer} />
                     <Route path='/inventory/add'  component='' />
                     <Route path='/inventory/:id'  component='' />
                     <Route path='/inventory/edit/:id'  component='' />
                 </Switch>
-            </BrowserRouter>
         </div>
     </>)
 }
--- src/Pages/Warehouse/WarehousePage.js
+++ src/Pages/Warehouse/WarehousePage.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Footer from "../../Components/Footer/Footer";
 import PageHeader from "../../Components/Header/Header";
 
@@ -8,14 +8,14 @@ function WarehousePage()  {
     return (<>
         <PageHeader isActive='Warehouse' />
         <div className="component-wrapper">
-            <BrowserRouter>
+
                 <Switch>
                     <Route path='/' exact component={Footer} />
+                    <Route path='/warehouse' exact component={Footer} />
                     <Route path='/warehouse/add'  component='' />
                     <Route path='/warehouse/:id'  component='' />
                     <Route path='/warehouse/edit/:id'  component='' />
                 </Switch>
-            </BrowserRouter>
         </div>
     </>)
 }
